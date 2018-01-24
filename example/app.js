d046b1ae8cab1a01658fb8a51c1691f11a1122af
Ben Gourley <bn@grly.me>
2018-01-24 15:12:52 +0000

    Style tweaks in example

--- example/app.js
+++ example/app.js
@@ -1,3 +1,5 @@
+/* global bugsnag, React, ReactDOM, bugsnag__react */
+
 // www.bugsnag.com
 // https://github.com/bugsnag/bugsnag-react/tree/master/example
 //
@@ -8,76 +10,79 @@
 
 // Initialize Bugsnag to begin tracking errors. Only an api key is required, but here are some other helpful configuration details:
 const bugsnagClient = bugsnag({
-    // get your own api key at bugsnag.com
-    apiKey: 'API_KEY',
+  // get your own api key at bugsnag.com
+  apiKey: 'API_KEY',
 
-    // if you track deploys or use source maps, make sure to set the correct version.
-    appVersion: '1.2.3',
+  // if you track deploys or use source maps, make sure to set the correct version.
+  appVersion: '1.2.3',
 
-    // Bugsnag can track the number of “sessions” that happen in your application, and calculate a crash rate for each release. This defaults to false.
-    autoCaptureSessions: true,
+  // Bugsnag can track the number of “sessions” that happen in your application, and calculate a crash rate for each release. This defaults to false.
+  autoCaptureSessions: true,
 
-    // defines the release stage for all events that occur in this app.
-    releaseStage: 'development',
+  // defines the release stage for all events that occur in this app.
+  releaseStage: 'development',
 
-    //  defines which release stages bugsnag should report. e.g. ignore staging errors.
-    notifyReleaseStages: [ 'development', 'production'],
+  //  defines which release stages bugsnag should report. e.g. ignore staging errors.
+  notifyReleaseStages: [ 'development', 'production' ],
 
-    // one of the most powerful tools in our library, beforeSend lets you evaluate, modify, add and remove data before sending the error to bugsnag. The actions here will be applied to *all* errors, handled and unhandled.
-    beforeSend: function (report) {
-      if (report.errorClass === 'Error' && report.severity === 'warning')  {
-        report.updateMetaData('example', {thing: "one"})
-      }
-      // note that if you return false from the beforeSend, this will cancel the entire error report.
-    },
+  // one of the most powerful tools in our library, beforeSend lets you evaluate, modify, add and remove data before sending the error to bugsnag. The actions here will be applied to *all* errors, handled and unhandled.
+  beforeSend: function (report) {
+    if (report.errorClass === 'Error' && report.severity === 'warning') {
+      report.updateMetaData('example', { thing: 'one' })
+    }
+    // note that if you return false from the beforeSend, this will cancel the entire error report.
+  },
 
-    // attached any user data you'd like to report.
-    user: {
-      name: "Katherine Johnson",
-      email: "kj@nasa.gov",
-      id: "0112358"
-    },
+  // attached any user data you'd like to report.
+  user: {
+    name: 'Katherine Johnson',
+    email: 'kj@nasa.gov',
+    id: '0112358'
+  },
 
-    // add any custom attributes relevant to your app. Note that metadata can be added here, in a specific notify() or in a beforeSend.
-    metaData: { company: {
-      name: "Hogwarts School of Witchcraft and Wizardry"
-      }
-    },
-    // N.B. our notifer automatically creates a metadata tab called "React" and populates it with component info.
+  // add any custom attributes relevant to your app. Note that metadata can be added here, in a specific notify() or in a beforeSend.
+  metaData: {
+    company: {
+      name: 'Hogwarts School of Witchcraft and Wizardry'
+    }
+  },
+  // N.B. our notifer automatically creates a metadata tab called "React" and populates it with component info.
 
-    // because this is a demo app, below extends the default of 10 notifications per pageload. click away!
-    maxEvents: 50
+  // because this is a demo app, below extends the default of 10 notifications per pageload. click away!
+  maxEvents: 50
 })
 
-
 class BadButton extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = { doARenderError: false }
   }
-  throwError = () => {
+
+  throwError () {
     try {
       // potentially buggy code goes here
-      //for this example, we're just throwing an error explcitly, but you do not need this syntax in your try clause.
-      throw new Error('Bad Thing!');
+      // for this example, we're just throwing an error explicitly, but you do not need this syntax in your try clause.
+      throw new Error('Bad Thing!')
     } catch (e) {
-      console.log("a handled error was sent to our dashboard.");
+      console.log('a handled error was sent to our dashboard.')
       bugsnagClient.notify(e, {
-        context: 'Don\'t worry - I handled it.'
+        context: 'Don’t worry - I handled it.'
       })
     }
   }
-  triggerRenderError = () => {
+
+  triggerRenderError () {
     this.setState({ doARenderError: true })
-  };
-  render() {
+  }
+
+  render () {
     return (
       <div>
         <p>
           <button onClick={this.throwError}>Handled error</button>
         </p>
         <p>
-          <button onClick={this.triggerRenderError}>Trigger a render error</button>
+          <button onClick={() => this.triggerRenderError()}>Trigger a render error</button>
           {this.state.doARenderError
             ? <span>{ this.state.doARenderError.non.existent.property }</span>
             : null
