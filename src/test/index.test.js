6396fa946313ffcf9bf8cf9da5845dffed452051
Martin Holman <martin308@gmail.com>
2018-04-02 14:41:39 -0700

    Test existing behaviour with jest

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,13 +1,55 @@
-/* global test, expect */
+/* global jest, expect, it, beforeEach, test */
 
-global.window = {}
+import React from 'react'
+import renderer from 'react-test-renderer'
+import index from '../'
 
-const p = require('../')
+const bugsnag = {
+  BugsnagReport: jest.fn(() => {
+    return {
+      updateMetaData: jest.fn()
+    }
+  }),
+  notify: jest.fn()
+}
+
+bugsnag.BugsnagReport.getStacktrace = jest.fn()
+
+const ErrorBoundary = index(React).init(bugsnag)
+
+beforeEach(() => {
+  bugsnag.notify.mockReset()
+})
 
 test('formatComponentStack(str)', () => {
   const str = `
   in BadButton
   in ErrorBoundary`
-  expect(p.formatComponentStack(str))
+  expect(index.formatComponentStack(str))
     .toBe(`in BadButton\nin ErrorBoundary`)
+})
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ErrorBoundary><p>test</p></ErrorBoundary>)
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+const BadComponent = () => {
+  throw Error('BadComponent')
+}
+
+it('renders correctly on error', () => {
+  const tree = renderer
+    .create(<ErrorBoundary><BadComponent /></ErrorBoundary>)
+    .toJSON()
+  expect(tree).toBe(null)
+})
+
+it('calls notify on error', () => {
+  renderer
+    .create(<ErrorBoundary><BadComponent /></ErrorBoundary>)
+    .toJSON()
+  expect(bugsnag.notify).toHaveBeenCalledTimes(1)
 })
